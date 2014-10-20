2a2ce2f07c8831a875bc8259e31dc1500413248b
Amazetbm <jtalley1@gmail.com>
2014-10-20 12:31:26 -0400

    Added date formatting

--- javascripts/libs/controllers/extensions_glue.js
+++ javascripts/libs/controllers/extensions_glue.js
@@ -22,6 +22,84 @@ var perfTarget = ' 4.00ms';
 var availLine =[];
 var perfLine = [];
 
+Date.prototype.format = function(format) {
+    var returnStr = '';
+    var replace = Date.replaceChars;
+    for (var i = 0; i < format.length; i++) {       var curChar = format.charAt(i);         if (i - 1 >= 0 && format.charAt(i - 1) == "\\") {
+            returnStr += curChar;
+        }
+        else if (replace[curChar]) {
+            returnStr += replace[curChar].call(this);
+        } else if (curChar != "\\"){
+            returnStr += curChar;
+        }
+    }
+    return returnStr;
+};
+
+Date.replaceChars = {
+    shortMonths: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+    longMonths: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+    shortDays: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+    longDays: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+
+    // Day
+    d: function() { return (this.getDate() < 10 ? '0' : '') + this.getDate(); },
+    D: function() { return Date.replaceChars.shortDays[this.getDay()]; },
+    j: function() { return this.getDate(); },
+    l: function() { return Date.replaceChars.longDays[this.getDay()]; },
+    N: function() { return this.getDay() + 1; },
+    S: function() { return (this.getDate() % 10 == 1 && this.getDate() != 11 ? 'st' : (this.getDate() % 10 == 2 && this.getDate() != 12 ? 'nd' : (this.getDate() % 10 == 3 && this.getDate() != 13 ? 'rd' : 'th'))); },
+    w: function() { return this.getDay(); },
+    z: function() { var d = new Date(this.getFullYear(),0,1); return Math.ceil((this - d) / 86400000); }, // Fixed now
+    // Week
+    W: function() { var d = new Date(this.getFullYear(), 0, 1); return Math.ceil((((this - d) / 86400000) + d.getDay() + 1) / 7); }, // Fixed now
+    // Month
+    F: function() { return Date.replaceChars.longMonths[this.getMonth()]; },
+    m: function() { return (this.getMonth() < 9 ? '0' : '') + (this.getMonth() + 1); },
+    M: function() { return Date.replaceChars.shortMonths[this.getMonth()]; },
+    n: function() { return this.getMonth() + 1; },
+    t: function() { var d = new Date(); return new Date(d.getFullYear(), d.getMonth(), 0).getDate() }, // Fixed now, gets #days of date
+    // Year
+    L: function() { var year = this.getFullYear(); return (year % 400 == 0 || (year % 100 != 0 && year % 4 == 0)); },   // Fixed now
+    o: function() { var d  = new Date(this.valueOf());  d.setDate(d.getDate() - ((this.getDay() + 6) % 7) + 3); return d.getFullYear();}, //Fixed now
+    Y: function() { return this.getFullYear(); },
+    y: function() { return ('' + this.getFullYear()).substr(2); },
+    // Time
+    a: function() { return this.getHours() < 12 ? 'am' : 'pm'; },
+    A: function() { return this.getHours() < 12 ? 'AM' : 'PM'; },
+    B: function() { return Math.floor((((this.getUTCHours() + 1) % 24) + this.getUTCMinutes() / 60 + this.getUTCSeconds() / 3600) * 1000 / 24); }, // Fixed now
+    g: function() { return this.getHours() % 12 || 12; },
+    G: function() { return this.getHours(); },
+    h: function() { return ((this.getHours() % 12 || 12) < 10 ? '0' : '') + (this.getHours() % 12 || 12); },
+    H: function() { return (this.getHours() < 10 ? '0' : '') + this.getHours(); },
+    i: function() { return (this.getMinutes() < 10 ? '0' : '') + this.getMinutes(); },
+    s: function() { return (this.getSeconds() < 10 ? '0' : '') + this.getSeconds(); },
+    u: function() { var m = this.getMilliseconds(); return (m < 10 ? '00' : (m < 100 ?
+'0' : '')) + m; },
+    // Timezone
+    e: function() { return "Not Yet Supported"; },
+    I: function() {
+        var DST = null;
+            for (var i = 0; i < 12; ++i) {
+                    var d = new Date(this.getFullYear(), i, 1);
+                    var offset = d.getTimezoneOffset();
+
+                    if (DST === null) DST = offset;
+                    else if (offset < DST) { DST = offset; break; }                     else if (offset > DST) break;
+            }
+            return (this.getTimezoneOffset() == DST) | 0;
+        },
+    O: function() { return (-this.getTimezoneOffset() < 0 ? '-' : '+') + (Math.abs(this.getTimezoneOffset() / 60) < 10 ? '0' : '') + (Math.abs(this.getTimezoneOffset() / 60)) + '00'; },
+    P: function() { return (-this.getTimezoneOffset() < 0 ? '-' : '+') + (Math.abs(this.getTimezoneOffset() / 60) < 10 ? '0' : '') + (Math.abs(this.getTimezoneOffset() / 60)) + ':00'; }, // Fixed now
+    T: function() { var m = this.getMonth(); this.setMonth(0); var result = this.toTimeString().replace(/^.+ \(?([^\)]+)\)?$/, '$1'); this.setMonth(m); return result;},
+    Z: function() { return -this.getTimezoneOffset() * 60; },
+    // Full Date/Time
+    c: function() { return this.format("Y-m-d\\TH:i:sP"); }, // Fixed now
+    r: function() { return this.toString(); },
+    U: function() { return this.getTime() / 1000; }
+};
+
 function initApp(){
 	var initButton = 'menu-link-digitalMedia';
 	buildout(initButton);
@@ -263,6 +341,7 @@ function loadSparkDyn(IDChain, chainData){
 function initTagButtons(fromQuery, toQuery){
 	var fromThis = fromQuery;
 	var toThis = toQuery;
+	
 	$('.theme-global-spark-link').click(function(){
 		var thisLocal = $(this).attr('id');
 		var thisSeq = $(this).attr('seq-loc');
@@ -275,12 +354,43 @@ function initTagButtons(fromQuery, toQuery){
 		var thisMarquee;
 		var fromThis = fromQuery;
 		var toThis = toQuery;
+		var past = new Date(fromThis);
+		var today = new Date(toThis);
+		var dd = today.getDate();
+		var mm = today.getMonth()+1;
+		var yyyy = today.getFullYear();
+		past = past.setDate(past.getDate()-30);
+		var pastMonth = new Date(past);
+		var p_dd = pastMonth.getDate();
+		var p_mm = pastMonth.getMonth()+1;
+		var p_yyyy = pastMonth.getFullYear();
+		var queryTo, queryFrom;
+		if(dd<10) {
+		    dd='0'+dd;
+		} 
+
+		if(mm<10) {
+		    mm='0'+mm;
+		} 
+		
+		if(p_dd<10) {
+		    p_dd='0'+p_dd;
+		} 
+
+		if(p_mm<10) {
+		    p_mm='0'+p_mm;
+		} 
+		
+		//today = mm+'/'+dd+'/'+yyyy;
+		//pastMonth = p_mm+'/'+p_dd+'/'+p_yyyy;
+		today = today.format('M d, Y');
+		pastMonth = pastMonth.format('M d, Y');
 		
 		if(thisLocal.indexOf('availtrend') > -1){
 			thisMarquee = thisLocal.split('-availtrend')[0];
 			thisMarquee = thisMarquee.split('-').join(' ');
 			thisMarquee = thisMarquee.toUpperCase();
-			chartDialog = $('<div id="'+thisLocal+'-avail-diag"><div class="chart-date-row">Base date range from: <span id="fromThis"></span> to: <span id="toThis"></span></div><div class="kpi-row"><div id="kpi-1" class="kpi-box"><div class="kpi-title">Availability</div><div class="kpi-actual">.</div><div class="kpi-indicator fa fa-arrow-up"></div><div class="target-label">Target</div><div class="kpi-target">.</div><div class="clear-this"></div></div><div id="kpi-3" class="kpi-box"><div class="kpi-title">Performance</div><div class="kpi-actual">.</div><div class="kpi-indicator fa fa-arrow-down"></div><div class="target-label">Target</div><div class="kpi-target">.</div><div class="clear-this"></div></div><div class="kpi-gauge-wrap"><div id="avail-gauge"class="kpi-gauge">.</div><div class="kpi-gauge-label">Availability</div><div class="clear-this"></div></div><div class="kpi-gauge-wrap"><div id="perf-gauge"class="kpi-gauge">.</div><div class="kpi-gauge-label">Performance</div><div class="clear-this"></div></div><div class="clear-this"></div></div><div id="'+thisLocal+'-avail-chart"></div><div id="'+thisLocal+'-long-chart" class="full-chart"></div><div class="full-box"><div id="'+thisLocal+'-avail-chart-slide" class="full-bar"></div></div><div class="chart-button-row" id="chart-buttons"></div></div>');
+			chartDialog = $('<div id="'+thisLocal+'-avail-diag"><div class="chart-date-row">Base date range: <span id="fromThis">'+pastMonth+'</span> - <span id="toThis">'+today+'</span></div><div class="kpi-row"><div id="kpi-1" class="kpi-box"><div class="kpi-title">Availability</div><div class="kpi-actual">.</div><div class="kpi-indicator fa fa-arrow-up"></div><div class="target-label">Target</div><div class="kpi-target">.</div><div class="clear-this"></div></div><div id="kpi-3" class="kpi-box"><div class="kpi-title">Performance</div><div class="kpi-actual">.</div><div class="kpi-indicator fa fa-arrow-down"></div><div class="target-label">Target</div><div class="kpi-target">.</div><div class="clear-this"></div></div><div class="kpi-gauge-wrap"><div id="avail-gauge"class="kpi-gauge">.</div><div class="kpi-gauge-label">Availability</div><div class="clear-this"></div></div><div class="kpi-gauge-wrap"><div id="perf-gauge"class="kpi-gauge">.</div><div class="kpi-gauge-label">Performance</div><div class="clear-this"></div></div><div class="clear-this"></div></div><div id="'+thisLocal+'-avail-chart"></div><div id="'+thisLocal+'-long-chart" class="full-chart"></div><div class="full-box"><div id="'+thisLocal+'-avail-chart-slide" class="full-bar"></div></div><div class="chart-button-row" id="chart-buttons"></div></div>');
 			dialogID = thisLocal+'-avail-chart';
 			longID = thisLocal+'-long-chart';
 			//thisName = thisName + '  AVAILABILITY';
@@ -288,7 +398,7 @@ function initTagButtons(fromQuery, toQuery){
 			thisMarquee = thisLocal.split('-perftrend')[0];
 			thisMarquee = thisMarquee.split('-').join(' ');
 			thisMarquee = thisMarquee.toUpperCase();
-			chartDialog = $('<div id="'+thisLocal+'-perf-diag"><div class="chart-date-row">Base date range from: <span id="fromThis"></span> to: <span id="toThis"></span></div><div class="kpi-row"><div id="kpi-1" class="kpi-box"><div class="kpi-title">Availability</div><div class="kpi-actual">.</div><div class="kpi-indicator fa fa-arrow-up"></div><div class="target-label">Target</div><div class="kpi-target">.</div><div class="clear-this"></div></div><div id="kpi-3" class="kpi-box"><div class="kpi-title">Performance</div><div class="kpi-actual">.</div><div class="kpi-indicator fa fa-arrow-down"></div><div class="target-label">Target</div><div class="kpi-target">.</div><div class="clear-this"></div></div><div class="kpi-gauge-wrap"><div id="avail-gauge"class="kpi-gauge">.</div><div class="kpi-gauge-label">Availability</div><div class="clear-this"></div></div><div class="kpi-gauge-wrap"><div id="perf-gauge"class="kpi-gauge">.</div><div class="kpi-gauge-label">Performance</div><div class="clear-this"></div></div><div class="clear-this"></div></div><div id="'+thisLocal+'-perf-chart"></div><div id="'+thisLocal+'-long-chart" class="full-chart"></div><div class="full-box"><div id="'+thisLocal+'-perf-chart-slide" class="full-bar"></div></div><div class="chart-button-row" id="chart-buttons"></div></div>');
+			chartDialog = $('<div id="'+thisLocal+'-perf-diag"><div class="chart-date-row">Base date range: <span id="fromThis">'+pastMonth+'</span> - <span id="toThis">'+today+'</span></div><div class="kpi-row"><div id="kpi-1" class="kpi-box"><div class="kpi-title">Availability</div><div class="kpi-actual">.</div><div class="kpi-indicator fa fa-arrow-up"></div><div class="target-label">Target</div><div class="kpi-target">.</div><div class="clear-this"></div></div><div id="kpi-3" class="kpi-box"><div class="kpi-title">Performance</div><div class="kpi-actual">.</div><div class="kpi-indicator fa fa-arrow-down"></div><div class="target-label">Target</div><div class="kpi-target">.</div><div class="clear-this"></div></div><div class="kpi-gauge-wrap"><div id="avail-gauge"class="kpi-gauge">.</div><div class="kpi-gauge-label">Availability</div><div class="clear-this"></div></div><div class="kpi-gauge-wrap"><div id="perf-gauge"class="kpi-gauge">.</div><div class="kpi-gauge-label">Performance</div><div class="clear-this"></div></div><div class="clear-this"></div></div><div id="'+thisLocal+'-perf-chart"></div><div id="'+thisLocal+'-long-chart" class="full-chart"></div><div class="full-box"><div id="'+thisLocal+'-perf-chart-slide" class="full-bar"></div></div><div class="chart-button-row" id="chart-buttons"></div></div>');
 			dialogID = thisLocal+'-perf-chart';
 			longID = thisLocal+'-long-chart';
 			//thisName = thisName + '  PERFORMANCE';
